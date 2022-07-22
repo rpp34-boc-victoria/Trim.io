1addc8de324a4351eb2117387194d5144f1aac7f
nicoleqi7 <nicole.q.daddy@gmail.com>
2022-07-22 00:38:30 -0700

    use stimulate data calculate bmi bfp

--- client/src/components/history/History.tsx
+++ client/src/components/history/History.tsx
@@ -1,11 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import MyBarChart from "./MyBarChart";
 import MyLineChart from "./MyLineChart";
 import "./History.scss";
 
+interface IDaliy {
+  user_id: string;
+  waterAmount: number;
+  weightAmount: number;
+  foodItems?: any;
+  entryDate?: Date;
+  bmi?: number;
+  bfp?: number;
+  age?: number;
+}
+
+type TGender = 0 | 1;
+
+enum EGender {
+  female=0,
+  male=1,
+}
+
+interface IUser {
+  gender: EGender;
+  weight: number;
+  height: number;
+  caloriesGoal: number;
+  waterGoal: number;
+  age: number;
+}
+
 const Weekly = () => {
+  const [userInfo, setUserInfo] = useState<IUser>({
+    gender: EGender.female,
+    weight: 50,
+    height: 1.7,
+    caloriesGoal: 500,
+    waterGoal: 8,
+    age: 27,
+  });
+  const [weeklyData, setWeeklyData] = useState<IDaliy[]>([
+    { user_id: "62da3e404afcce3152212f47", waterAmount: 8, weightAmount: 49 },
+    { user_id: "62da3e404afcce3152212f47", waterAmount: 5, weightAmount: 49 },
+    { user_id: "62da3e404afcce3152212f47", waterAmount: 8, weightAmount: 48 },
+    { user_id: "62da3e404afcce3152212f47", waterAmount: 3, weightAmount: 49 },
+  ]);
+
+  useEffect(() => {
+    //发起请求 ， 成功后根据获取的数据计算bmi，bfp
+    const tempData = [...weeklyData];
+    const { weight, height, gender, age } = userInfo;
+    tempData.forEach((item) => {
+      item.bmi = Number((weight / (height * height)).toFixed(2));
+      if (gender === EGender.female) {
+        item.bfp = 1.2 * item.bmi + 0.23 * age - 5.4;
+      } else if (gender === EGender.male) {
+        item.bfp = 1.2 * item.bmi + 0.23 * age - 16.2;
+      }
+      
+    });
+
+    console.log(weeklyData)
+    setWeeklyData(tempData)
+    console.log(weeklyData)
+  },[]);
+
   return (
     <Box className="weekly">
       <Box className="calories chart_item">
@@ -23,7 +84,7 @@ const Weekly = () => {
       <Box className="weight chart_item ">
         <Typography className="title">Weight</Typography>
         <Box className="chart_wrap">
-          <MyLineChart/>
+          <MyLineChart />
         </Box>
       </Box>
       <Box className="BMI chart_item ">
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,11 @@
-import express from 'express';
-import path from 'path';
-import * as dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+// import connection from "./db/connect"
+import * as dotenv from "dotenv";
+import { userEntriesModel,dailyEntriesModel , foodEntriesModel} from "./db/schema.models";
+import db from "../server/db/connect";
+import dayjs from 'dayjs'
+import { log } from "console";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -9,7 +14,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+db();
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -21,4 +28,34 @@ app.get("/api/hello", (req, res) => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.get("/api/getWeekly", (req, res) => {
+  // console.log(req, res)
+  console.log(1)
+  // const start = dayjs().subtract(7, 'day')
+  // var query = {
+  //   createTime: {
+  //     $gt: start.toISOString(),
+  //     $lt: new Date(new Date().toISOString()),
+  //   },
+  // };
+  // 再使用mongodb查询调用这个query作为查询条件
+  const results = dailyEntriesModel.find({});
+  res.send(results)
+});
+
+app.get("/api/register", (req, res) => {
+  let foodItems=new foodEntriesModel({
+    label: 'apply',
+    nutrients: 'test'
+  })
+  let daily = new dailyEntriesModel({
+    user_id:'62da35785754355239a691f3',
+    foodItems,
+    waterAmount:8,
+    weightAmount:50
+  })
+  daily.save()
+  res.send({ message: "Hello" });
 });
