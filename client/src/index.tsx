32aa68e2f00e65c5e239fcc10b09738cebfe5e37
Derzan Chiang <derzanchiang1800@gmail.com>
2022-07-23 09:57:50 -0700

    add service worker basic files and install workbox

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,10 +5,12 @@ import App from './components/App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -16,3 +18,8 @@ root.render(
     <App />
   </ThemeProvider>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
