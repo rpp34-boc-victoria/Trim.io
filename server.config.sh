a9670d77e08ba11dcff7bc5f10e65eb8ab237195
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-07-23 17:01:08 -0400

    :q!

--- server.config.sh
+++ server.config.sh
@@ -1,12 +1,18 @@
+# Update instance
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
+
+# Git Short Hand Setup
 git config --global alias.ci commit
 git config --global alias.st status
+
+# Install MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
+
+# Install NVM and specified Node Version
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
 source ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
@@ -14,8 +20,25 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16.15.1
 node -e "console.log('Running Node.js ' + process.version)"
+
+# Setting up pm2 to run the server as a service on start up
 npm install pm2@latest -g
-#pm2 start dist/server.js
-#sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.15.1/bin /home/ubuntu/.nvm/versions/node/v16.15.1/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-#pm2 save
-#pm2 status
+# pm2 start dist/server.js
+# sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.15.1/bin /home/ubuntu/.nvm/versions/node/v16.15.1/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# pm2 save
+# pm2 status
+
+# Creating 1 GB Swap File Memory to allow disk caching during build, which is needed to run the npm build for create-react-app
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+# logging created file
+ls -lh /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+# Logging the swap file
+sudo swapon --show
+free -h
+# Making it permanet on boot up
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# Resource :https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-18-04
