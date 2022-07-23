34723020a93cc499a1858e6969014b32445a2f23
Derzan Chiang <derzanchiang1800@gmail.com>
2022-07-23 10:29:09 -0700

    basic serverworker

--- client/src/serviceWorkerRegistration.ts
+++ client/src/serviceWorkerRegistration.ts
@@ -24,7 +24,9 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // for production
+  // process.env.NODE_ENV === 'production' &&
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -35,7 +37,10 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swFileName = process.env.NODE_ENV === 'production'
+        ? 'service-worker.js'
+        : 'custom-sw.js';
+      const swUrl = `${process.env.PUBLIC_URL}/${swFileName}`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
