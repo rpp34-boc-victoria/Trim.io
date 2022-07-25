a0faba97f10e1c458e3d89ad296688fd1cbaedba
slava000 <vstarikov@gmail.com>
2022-07-25 18:15:42 -0300

    Add components to form

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,9 +5,14 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import History from './history/History';
+import AddEntry from './dailyEntries/AddEntry';
+import Incrementer from './dailyEntries/Incrementer';
+import {ThemeProvider} from '@mui/material/styles';
+import { Divider } from '@mui/material';
+import theme from '../theme';
+
 
 function Copyright() {
-
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
@@ -21,15 +26,21 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
-        </Typography>
-        <ProTip />
-        <History />
-        <Copyright />
-      </Box>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App example with TypeScript
+          </Typography>
+          <ProTip />
+          <History />
+          <Incrementer labelText='Water (cups)' />
+          <Divider sx={{mb: '16px'}} />
+          <Incrementer labelText='Body weight' />
+          <AddEntry></AddEntry>
+          <Copyright />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
