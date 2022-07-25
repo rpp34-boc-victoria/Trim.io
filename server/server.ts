44af604ad6e23c7e63a3a3ce484b0c3e110e26fa
Derzan Chiang <derzanchiang1800@gmail.com>
2022-07-25 16:53:43 -0700

    async working with node schedule

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import webpush from 'web-push';
+import schedule from 'node-schedule';
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -24,16 +25,28 @@ app.post('/notifications/subscribe', (req, res) => {
 
   console.log('SUB', subscription)
 
+  res.status(200).json({'success': true})
+});
+
+const job = schedule.scheduleJob('*/10 * * * * *', async () => {
   const payload = JSON.stringify({
     title: 'Hello!',
     description: 'It ',
   })
-
-  webpush.sendNotification(subscription, payload)
+  const sub = {
+    endpoint: 'https://fcm.googleapis.com/fcm/send/fG20vaUTLFw:APA91bFZj5P2gR3tOAr-Gv8lVSUy5kmM_4V1WPHy3y2fnWPENOzRkm87TOhLJLWaaIcAPSS3iMdjkfcCNRNU13IVLJ4xyXUNdbpma9Ntpy-f4BOqVjPU3rm7hRXKBZyivBRn2_P_N91p',
+    expirationTime: null,
+    keys: {
+      p256dh: 'BGSGUOoqYiynV-WBTR03ZlnOSZxw5KEQK6UJQyZyWi4zRuqXfD3ZdEc6rNJMUNVg7KS7lWABusk6s3EoB7xWt7w',
+      auth: 'z8kYHIdEL3Oby22LQ6fcFw'
+    }
+  }
+  webpush.sendNotification(sub, payload)
     .then(result => console.log('AYOOO', result))
     .catch(e => console.log(e.stack))
-  res.status(200).json({'success': true})
 });
+
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
