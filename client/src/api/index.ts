b9d336c9bc7c17d6c61b5d3dc183bfad2ebccb44
nicoleqi7 <nicole.q.daddy@gmail.com>
2022-07-27 20:24:49 -0700

    adjusted code

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,30 +1,21 @@
 import axios from "axios";
 
-
 export const apiGet = async (path: string, params: any = {}) => {
   try {
     let result = await axios.get(path, params);
-    if(result.status ===200) {
-      return result.data
-    }else {
-      throw new Error('data query failure')
-    }
+    return result.data;
   } catch (error) {
-    alert(error)
-    // return error
+    console.log(error);
+    throw new Error("data query failure");
   }
 };
 
 export const apiPost = async (path: string, data: any = {}) => {
   try {
     let result = await axios.post(path, data);
-    if(result.status ===200) {
-      return result.data
-    }else {
-      throw new Error('data query failure')
-    }
+    return result.data;
   } catch (error) {
-    alert(error)
-    // return error
+    console.log(error);
+    throw new Error("data query failure");
   }
 };
