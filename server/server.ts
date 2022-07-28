2fbb408503106be215235cb43804c53f42569d73
Derzan Chiang <derzanchiang1800@gmail.com>
2022-07-27 18:12:27 -0700

    wip pr

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
-import webpush from 'web-push';
-import schedule from 'node-schedule';
+import { startSchedule } from './scheduleNotification';
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -13,8 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
 
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello" });
@@ -28,30 +25,6 @@ app.post('/notifications/subscribe', (req, res) => {
   res.status(200).json({'success': true})
 });
 
-// loop through all subscript and process/send out custom notifications
-const job = schedule.scheduleJob('*/10 * * * * *', async () => {
-
-  const payload = JSON.stringify({
-    title: 'Hello!',
-    description: 'It ',
-  })
-
-  const sub = {
-    endpoint: 'https://fcm.googleapis.com/fcm/send/fG20vaUTLFw:APA91bFZj5P2gR3tOAr-Gv8lVSUy5kmM_4V1WPHy3y2fnWPENOzRkm87TOhLJLWaaIcAPSS3iMdjkfcCNRNU13IVLJ4xyXUNdbpma9Ntpy-f4BOqVjPU3rm7hRXKBZyivBRn2_P_N91p',
-    expirationTime: null,
-    keys: {
-      p256dh: 'BGSGUOoqYiynV-WBTR03ZlnOSZxw5KEQK6UJQyZyWi4zRuqXfD3ZdEc6rNJMUNVg7KS7lWABusk6s3EoB7xWt7w',
-      auth: 'z8kYHIdEL3Oby22LQ6fcFw'
-    }
-  }
-
-  webpush.sendNotification(sub, payload)
-    .then(result => console.log('AYOOO', result))
-    .catch(e => console.log(e.stack))
-});
-
-
-
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
@@ -59,3 +32,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+startSchedule();
