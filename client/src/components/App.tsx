da143290576baa99ef80a56ce3bde600a3196496
Zhaowei <ding.zhaowei.ding@gmail.com>
2022-07-30 03:36:43 -0400

    aligning titile to center, adding the app name

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
-import ProTip from "./ProTip";
+// import ProTip from "./ProTip";
 import Weekly from "./history/Weekly";
 import { getDaily } from '../api';
 import Daily from "./dashboard/Daily";
@@ -50,10 +50,10 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
+        <Typography variant="h3" component="h1" gutterBottom align="center">
+          Trim.io
         </Typography>
-        <ProTip />
+        {/* <ProTip /> */}
         {/* <History /> */}
         <Box className="history">
           <Box className="tab_wrap">
