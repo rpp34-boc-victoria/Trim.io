2e12b71551e23c16794afe7b607761c4d667aed2
nicoleqi7 <nicole.q.daddy@gmail.com>
2022-08-03 19:38:23 -0700

    test

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -86,7 +86,7 @@ export default function App(props: any) {
                   }`}
                 onClick={() => handleChangeTab("daily")}
               >
-                Day
+                Daily
               </Typography>
               <Typography
                 className={`tab_item ${activeIndex === "weekly" ? "active" : ""
