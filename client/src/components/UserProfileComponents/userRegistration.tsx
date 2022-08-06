703439351378239a02907a3543140e6ae681705e
Ahmad Almomani <92074567+ahmadalmomani96@users.noreply.github.com>
2022-08-05 19:24:27 -0700

    Update 2

--- client/src/components/UserProfileComponents/userRegistration.tsx
+++ client/src/components/UserProfileComponents/userRegistration.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { apiPost } from "../../api";
+// import * as React from "react";
+// import { apiPost } from "../../api";
 import "./userRegistration.scss";
 import { Typography, Input, Box } from "@mui/material";
 import Select from "react-select";
